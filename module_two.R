cdbe8097a579c932caeb87a1f7ac9f5cc4a7a277
Jatin <jatinrohilla69@gmail.com>
2017-11-14 16:51:22 +0530

    module two

--- module_two.R
+++ module_two.R
@@ -5,10 +5,124 @@ source("module_two_core.R")
 ## ======================= UTILITY LIST ==========================
 
 module_two_list<-c( "Correlation",
-                    "Multiple Linear Regression",
-                    "Logistic Regression"
+                    "Multiple Linear Regression"
 )
 
 
 ## ========================= I/O FUNCTIONS ==============================
+
+
+## ======================== Correlation ==================================
+
+my_cor_input<-function(){
+  tagList(
+    textInput('my_cor_input_dataOne', 'Enter a vector (comma delimited)', "8,9,7,6,13,7,11,12"),
+    textInput('my_cor_input_dataTwo', 'Enter a vector (comma delimited)', "35,49,27,33,60,21,45,51")
+  )
+}
+
+
+my_cor_output<-function(){
+  tagList(
+    renderPrint({
+      
+      # Preparing data
+      x <- as.numeric(unlist(strsplit(input$my_cor_input_dataOne,",")))
+      y <- as.numeric(unlist(strsplit(input$my_cor_input_dataTwo,",")))
+      
+      #my_data<-sort(my_data)
+      # Nicely Display the source data
+      cat("For dataset :\n")
+      
+      cat("\nX : ")
+      if(length(x)<=12){
+        cat(x)
+      }
+      else{
+        cat(head(x,4),"...",tail(x,4))
+      }
+      
+      
+      cat("\ny : ")
+      if(length(y)<=12){
+        cat(y)
+      }
+      else{
+        cat(head(y,4),"...",tail(y,4))
+      }
+      
+      result<-my_cor(x,y)
+      # Print the result
+      cat(sprintf("\n\nCorrelation : \n%s",result))
+      
+    })
+  )
+}
+
+
+## ========================== Multi Linear Regression ======================================
+
+
+my_multi_linear_regression_input<-function(){
+  tagList(
+    textInput('my_multi_linear_regression_input_dataOne', 'Enter X1', "3,2,4,2,3,2,5,4"),
+    textInput('my_multi_linear_regression_input_dataTwo', 'Enter X2', "2,1,3,1,2,2,3,2"),
+    textInput('my_multi_linear_regression_input_dataThree', 'Enter Y', "78800,7430,83800,74200,79700,74900,88400,82900")
+  )
+}
+
+
+
+my_multi_linear_regression_output<-function(){
+  tagList(
+    renderPrint({
+      
+      # Preparing data
+      x1 <- as.numeric(unlist(strsplit(input$my_multi_linear_regression_input_dataOne,",")))
+      x2 <- as.numeric(unlist(strsplit(input$my_multi_linear_regression_input_dataTwo,",")))
+      y <- as.numeric(unlist(strsplit(input$my_multi_linear_regression_input_dataThree,",")))
+      
+      #my_data<-sort(my_data)
+      # Nicely Display the source data
+      cat("For equations :\n")
+      
+      cat("\nX1 : ")
+      if(length(x1)<=12){
+        cat(x1)
+      }
+      else{
+        cat(head(x1,4),"...",tail(x1,4))
+      }
+      
+      
+      cat("\nX2 : ")
+      if(length(x2)<=12){
+        cat(x2)
+      }
+      else{
+        cat(head(x2,4),"...",tail(x2,4))
+      }
+      
+      cat("\nY : ")
+      if(length(y)<=12){
+        cat(y)
+      }
+      else{
+        cat(head(y,4),"...",tail(y,4))
+      }
+      
+      cat(sprintf("\n\nMultiple Linear Regression : \n"))
+      result<-my_multi_linear_regression(x1,x2,y)
+      if(result=="Error : X1 and X2 must be of same length."){
+        cat(sprintf(result))
+      }
+      else{
+        # Print the result
+        cat(sprintf("Y =        %.6f \n  + (x1) * %.6f \n  + (x2) * %.6f",result[1],result[2],result[3]))  
+      }
+      
+    })
+  )
+  
+}
 
--- module_two_core.R
+++ module_two_core.R
@@ -1,2 +1,56 @@
-# ================ CORE FUNCTIONS FOR MODULE ONE =================
+# ================ CORE FUNCTIONS FOR MODULE TWO =================
 
+
+## =========================== Correlation ======================================
+
+my_cor<-function(x,y) {
+  
+  # x and y must be multile data with same length
+  
+  # In built 
+  # x<-c(8,9,7,6,13,7,11,12)
+  # y<-c(35,49,27,33,60,21,45,51)
+  # cor(x,y)
+  if(length(x)==length(y)){
+    n<-length(x)
+    r<-( n * sum(x**2) - (sum(x)**2) ) * ( n * sum(y**2) - (sum(y)**2) )  
+    r<-sqrt(r)
+    r<-1/r
+    r<-( n * sum(x*y) - sum(x) *sum(y) ) * r
+    
+    # 6 digit precision
+    result <- formatC(r,digits = 6,format = "f")
+    return(result)
+  }
+  else{
+    return("Error : X and Y must be of same length.")
+  }
+}
+
+
+## ========================== Multi Linear Regression ======================================
+
+my_multi_linear_regression<-function(x1 , x2 , y ) {
+  
+  
+  if(length(x1)==length(x2)){
+    
+    xMatrix<-cbind(c(length(x1),sum(x1),sum(x2)) , 
+                   c(sum(x1),sum(x1**2),sum(x1*x2)) , 
+                   c(sum(x2),sum(x1*x2),sum(x2**2)))
+    
+    xyMatrix<-cbind(c(sum(y),
+                      sum(x1*y),
+                      sum(x2*y)))
+    
+    result<-solve(xMatrix,xyMatrix)    #matrix inverse , then multiply with inverse and save final result in result
+    
+    return(c(result[1][1] ,result[2][1],  result[3][1]))
+  }
+  else{
+    return("Error : X1 and X2 must be of same length.")
+  }
+  
+  
+
+  }
